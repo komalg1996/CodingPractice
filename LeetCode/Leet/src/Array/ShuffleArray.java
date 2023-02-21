c60e233279c486809f73a63b540a037ec6adb5d2
Komal Deepak Ghorpade <121026850+komalg2723@users.noreply.github.com>
2023-02-22 00:41:15 +0530

    day1

---
+++ LeetCode/Leet/src/Array/ShuffleArray.java
@@ -0,0 +1,23 @@
+package Array;
+
+import java.util.Arrays;
+
+public class ShuffleArray {
+	
+ public static int[] shuffle(int[] nums, int n) {
+     int[] ans = new int[2 * n];
+     for (int i = 0; i < n; i++) {
+         ans[2 * i] = nums[i];
+         ans[2 * i + 1] = nums[i + n];
+     }
+     return ans;
+ }
+        
+	public static void main(String[] args) {
+		int n=3;
+		int [] nums= {2,5,1,3,4,7};	//op:[2,3,5,4,1,7] 
+		int [] output= shuffle(nums,n);
+		System.out.println(Arrays.toString(output));
+	}
+
+}
